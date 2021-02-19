8281aeed88d5ff744d7004b128489620a37d1d0e
AnnekimBanchereau <annekim.banchereau@gmail.com>
2021-02-19 11:40:39 +0100

    clean code

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Loader } from 'semantic-ui-react';
 
-const LoaderRepos = () => <Loader active inline='centered' />;
+const LoaderRepos = () => <Loader active inline="centered" />;
 
 export default LoaderRepos;
